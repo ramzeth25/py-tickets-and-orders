2fc0f4e0e4957388616cb93ccbc3c29bae205cbc
ramzeth <ramzeth25@gmail.com>
2026-05-03 10:03:38 +0300

    Solution_3

--- services/user.py
+++ services/user.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from db.models import User
+from django.contrib.auth.base_user import AbstractBaseUser
 
 
 def create_user(
@@ -8,7 +8,7 @@ def create_user(
         email: str | None = None,
         first_name: str | None = None,
         last_name: str | None = None
-) -> User:
+) -> AbstractBaseUser:
     user = get_user_model().objects.create_user(username, password)
 
     if email:
@@ -22,7 +22,7 @@ def create_user(
     return user
 
 
-def get_user(user_id: int) -> User:
+def get_user(user_id: int) -> AbstractBaseUser:
     return get_user_model().objects.get(pk=user_id)
 
 
@@ -33,7 +33,7 @@ def update_user(
         email: str = None,
         first_name: str = None,
         last_name: str = None
-) -> User:
+) -> AbstractBaseUser:
     user = get_user(user_id)
 
     if username:
